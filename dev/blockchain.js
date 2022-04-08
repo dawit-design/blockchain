3c98b9f8f1d6f9f8b2b7725dd1c09dbb506901d6
dawit-design <dawitawegchow@gmail.com>
2022-04-07 23:56:32 -0400

    created new transactions method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = []
 
 }
 
@@ -8,15 +8,30 @@ Blockchain.prototype.createNeWBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transaction: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length - 1]
+}
+
+Blockchain.prototype.createNewTransactions = function(amount, sender, recipient){
+ const newTransaction = {
+     amount: amount,
+     sender: sender,
+     recipient: recipient
+ };
+ this.pendingTransactions.push(newTransaction);
+
+ return this.getLastBlock()['index'] + 1
 }
 module.exports = Blockchain;
